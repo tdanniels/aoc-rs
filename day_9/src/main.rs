301d017f356f9cdd2a682eb5292c869628beb94f
Travis Danniels <tdanniels@gmail.com>
2022-04-08 20:49:57 -0700

    Day 9 part 2

--- day_9/src/main.rs
+++ day_9/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::{BinaryHeap, HashSet, VecDeque};
 use std::error;
 use std::fmt::Debug;
 use std::fs::File;
@@ -54,11 +55,15 @@ impl Grid<u32> {
 
     /// The elements of the output vector are respectively
     /// N W E S relative to the input coordinates.
-    fn neighbourhood(&self, row_idx: usize, col_idx: usize) -> Option<Vec<Option<u32>>> {
+    fn neighbourhood(
+        &self,
+        row_idx: usize,
+        col_idx: usize,
+    ) -> Option<Vec<((usize, usize), Option<u32>)>> {
         if row_idx >= self.num_rows || col_idx >= self.num_cols {
             return None;
         }
-        let mut out: Vec<Option<u32>> = Vec::new();
+        let mut out: Vec<((usize, usize), Option<u32>)> = Vec::new();
 
         for (cond, (r, c)) in [
             (row_idx > 0, (row_idx.overflowing_sub(1).0, col_idx)),
@@ -67,43 +72,91 @@ impl Grid<u32> {
             (row_idx < self.num_rows - 1, (row_idx + 1, col_idx)),
         ] {
             if cond {
-                out.push(self.at(r, c));
+                out.push(((r, c), self.at(r, c)));
             } else {
-                out.push(None);
+                out.push(((r, c), None));
             }
         }
         Some(out)
     }
+
+    fn find_low_points(&self) -> Vec<((usize, usize), u64)> {
+        let mut out = Vec::new();
+        for i in 0..self.num_rows {
+            for j in 0..self.num_cols {
+                let centre = self.at(i, j).expect("Bad centrepoint coords?");
+                if self
+                    .neighbourhood(i, j)
+                    .expect("Bad neighbourhood coords?")
+                    .iter()
+                    .all(|&x| {
+                        if let Some(neighbour_height) = x.1 {
+                            centre < neighbour_height
+                        } else {
+                            true
+                        }
+                    })
+                {
+                    out.push(((i, j), centre as u64));
+                }
+            }
+        }
+        out
+    }
+}
+
+/// Assumes that starting_point is a low point. Should fix this implicit assumption.
+fn get_basin_size(grid: &Grid<u32>, starting_point: &(usize, usize)) -> u64 {
+    let mut q: VecDeque<(usize, usize)> = VecDeque::new();
+    let mut explored: HashSet<(usize, usize)> = HashSet::new();
+    explored.insert(*starting_point);
+    q.push_back(*starting_point);
+    while q.len() > 0 {
+        let v = q.pop_front().unwrap();
+        for neighbour in grid.neighbourhood(v.0, v.1).unwrap() {
+            if !neighbour.1.is_some() {
+                continue;
+            }
+            let neighbour_height = neighbour.1.unwrap();
+            if neighbour_height <= grid.at(v.0, v.1).unwrap() || neighbour_height == 9 {
+                continue;
+            }
+            if explored.get(&neighbour.0).is_none() {
+                explored.insert(neighbour.0);
+                q.push_back(neighbour.0);
+            }
+        }
+    }
+    explored.len() as u64
 }
 
 fn part1(grid: &Grid<u32>) -> DResult<u64> {
     let mut accum: u64 = 0;
-    for i in 0..grid.num_rows {
-        for j in 0..grid.num_cols {
-            let centre = grid.at(i, j).ok_or("Bad centerpoint coords?")?;
-            if grid
-                .neighbourhood(i, j)
-                .ok_or("Bad neighbourhood coords?")?
-                .iter()
-                .all(|&x| {
-                    if let Some(neighbour) = x {
-                        centre < neighbour
-                    } else {
-                        true
-                    }
-                })
-            {
-                accum += centre as u64 + 1
-            }
-        }
+    for p in grid.find_low_points() {
+        accum += p.1 as u64 + 1
     }
     Ok(accum)
+}
+
+fn part2(grid: &Grid<u32>) -> DResult<u64> {
+    let low_points = grid.find_low_points();
+
+    Ok(low_points
+        .iter()
+        .map(|x| get_basin_size(grid, &x.0))
+        .collect::<BinaryHeap<u64>>()
+        .into_sorted_vec()
+        .iter()
+        .rev()
+        .take(3)
+        .product())
 }
 
 fn main() -> DResult<()> {
     let grid: Grid<u32> = Grid::from_file(FILENAME)?;
 
     println!("Part 1: {}", part1(&grid)?);
+    println!("Part 2: {}", part2(&grid)?);
 
     Ok(())
 }
