abfed52b8b2d55c6774cbab5223d7423137e14ef
Travis Danniels <tdanniels@gmail.com>
2022-04-19 23:58:24 -0700

    Day 14

---
+++ day_14/src/main.rs
@@ -0,0 +1,123 @@
+use aoc_util::{failure, AocResult};
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{self, BufRead};
+
+static FILENAME: &str = "input.txt";
+
+type Pair = [u8; 2];
+
+type Rules = HashMap<String, String>;
+
+fn parse_input(filename: &str) -> AocResult<(String, Rules)> {
+    let file = File::open(filename)?;
+    let mut lines = io::BufReader::new(file).lines();
+    let mut rules: Rules = Rules::new();
+
+    let template: String = lines.next().ok_or("No template?")??;
+    if !template.is_ascii() {
+        return failure(&format!("Template {} isn't ascii", template));
+    }
+
+    if lines.next().ok_or("Nothing after template?")?? != "" {
+        return failure("No empty line between template and rules?");
+    }
+    for line in lines {
+        let line = line?;
+        let mut split = line.split("->");
+
+        let pair = split.next().ok_or("No pair?")?.trim();
+        if !pair.is_ascii() {
+            return failure(&format!("Pair {} isn't ascii", pair));
+        }
+
+        let insertion = split.next().ok_or("No insertion?")?.trim();
+        if !insertion.is_ascii() {
+            return failure(&format!("Insertion {} isn't ascii", insertion));
+        }
+
+        rules.insert(pair.to_string(), insertion.to_string());
+    }
+    Ok((template, rules))
+}
+
+fn step_pair_counts(
+    pair_counts: &HashMap<Pair, usize>,
+    pair_productions: &HashMap<Pair, [Pair; 2]>,
+) -> AocResult<HashMap<Pair, usize>> {
+    let mut out: HashMap<Pair, usize> = HashMap::new();
+    for (p, c) in pair_counts {
+        let production = pair_productions
+            .get(p)
+            .ok_or(format!("No production for pair {:?}?", p))?;
+        for i in 0..2 {
+            let out_entry = out.entry(production[i]).or_insert(0);
+            *out_entry += c;
+        }
+    }
+    Ok(out)
+}
+
+fn solve(template: &str, rules: &Rules, n_steps: u32) -> AocResult<usize> {
+    let mut pair_productions: HashMap<Pair, [Pair; 2]> = HashMap::new();
+    for rule in rules {
+        let rule_bytes = rule.0.as_bytes();
+        let insertion_byte = rule.1.as_bytes()[0];
+        pair_productions.insert(
+            rule_bytes.try_into()?,
+            [
+                [rule_bytes[0], insertion_byte],
+                [insertion_byte, rule_bytes[1]],
+            ],
+        );
+    }
+
+    let mut pair_counts: HashMap<Pair, usize> = HashMap::new();
+    for p in template.as_bytes().windows(2) {
+        let entry = pair_counts.entry(p.try_into()?).or_insert(0);
+        *entry += 1;
+    }
+    for _ in 0..n_steps {
+        pair_counts = step_pair_counts(&pair_counts, &pair_productions)?;
+    }
+
+    let mut element2count: HashMap<u8, usize> = HashMap::new();
+    pair_counts.iter().for_each(|(p, c)| {
+        for i in 0..2 {
+            let entry = element2count.entry(p[i]).or_insert(0);
+            *entry += c;
+        }
+    });
+
+    // Fix-up: we've counted every element twice except the very first and very last
+    // elements in the sequence, which have been counted 2n-1 times.
+    for index in [0usize, template.len() - 1] {
+        let count = element2count
+            .get_mut(&template.as_bytes()[index])
+            .ok_or(format!("No {}th elem?", index))?;
+        *count += 1;
+    }
+    for (_, c) in element2count.iter_mut() {
+        *c /= 2;
+    }
+
+    let max_count = element2count
+        .iter()
+        .max_by_key(|&(_, v)| v)
+        .ok_or("No max?")?
+        .1;
+    let min_count = element2count
+        .iter()
+        .min_by_key(|&(_, v)| v)
+        .ok_or("No min?")?
+        .1;
+    Ok(max_count - min_count)
+}
+
+fn main() -> AocResult<()> {
+    let (template, rules) = parse_input(FILENAME)?;
+    println!("Part 1: {}", solve(&template, &rules, 10)?);
+    println!("Part 2: {}", solve(&template, &rules, 40)?);
+
+    Ok(())
+}
